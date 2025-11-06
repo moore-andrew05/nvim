c3206f867bdb55bcb43a44e49ceb14201825b2d8
Andrew Moore <moore.andrew0598@gmail.com>
2025-11-05 17:04:02 -0700

    Some snippet changes

--- lua/snippets/all.lua
+++ lua/snippets/all.lua
@@ -1,14 +1,17 @@
-return {
-  -- A snippet that expands the trigger "hi" into the string "Hello, world!".
-  require("luasnip").snippet(
-    { trig = "hi" },
-    { t("Hello, world!") }
-  ),
+-- Abbreviations used in this article and the LuaSnip docs
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local d = ls.dynamic_node
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
 
-  -- To return multiple snippets, use one `return` statement per snippet file
-  -- and return a table of Lua snippets.
-  require("luasnip").snippet(
-    { trig = "foo" },
-    { t("Another snippet.") }
-  ),
+vim.keymap.set('n', '<Leader>L',
+    '<Cmd>lua require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/lua/snippets/"})<CR>')
+
+return {
 }
--- test.py
+++
@@ -1,6 +0,0 @@
-import math
-
-
-def add(a: int, b: int):
-    return a + b
-add(a, b):
